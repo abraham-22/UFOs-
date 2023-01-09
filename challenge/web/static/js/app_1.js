7a1f737aa418cc81179c668ffa5314406009bbee
abraham-22 <abramabebe@gmail.com>
2023-01-09 03:28:37 -0600

    Updated code

--- challenge/web/static/js/app_1.js
+++ challenge/web/static/js/app_1.js
@@ -58,15 +58,17 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    let filters = tableData;
-    console.log(filters);
+    let filteredData = tableData;
+    console.log(filteredData);
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    filterTable()
+    Object.entries(filters).forEach(([key,value])=> {
+      filteredData =  filteredData.filter(row => row[key]=== value);
+    });  
   
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filters);
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
